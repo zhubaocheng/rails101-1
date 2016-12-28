6747a477442f61bdbb0a01f14b8620d4b9d127cc
zhubaocheng <imzhubaocheng@gmail.com>
2016-12-28 23:58:11 +0800

    implement groups#new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,14 +12,17 @@ class GroupsController < ApplicationController
   end
 
   def edit
-  @group = Group.find(params[:id])
+    @group = Group.find(params[:id])
   end
 
   def create
     @group = Group.new(group_params)
-    @group.save
 
-    redirect_to groups_path
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def update
@@ -28,6 +31,13 @@ class GroupsController < ApplicationController
     @group.update(group_params)
 
     redirect_to groups_path, notice: "Update Success"
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to groups_path
   end
 
   private
